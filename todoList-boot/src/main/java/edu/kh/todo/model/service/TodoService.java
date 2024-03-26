0d3936fc7560680cab3172a69c467c8500de45d2
haechankim <haechan0202@naver.com>
2024-03-26 16:47:05 +0900

    20240326 김해찬 수업내용

--- todoList-boot/src/main/java/edu/kh/todo/model/service/TodoService.java
+++ todoList-boot/src/main/java/edu/kh/todo/model/service/TodoService.java
@@ -1,5 +1,6 @@
 package edu.kh.todo.model.service;
 
+import java.util.List;
 import java.util.Map;
 
 import edu.kh.todo.model.dto.Todo;
@@ -57,6 +58,12 @@ public interface TodoService {
 	 * @return completeCount
 	 */
 	int getCompleteCount();
+
+	
+	/** 할 일 목록 조회
+	 * @return todolist
+	 */
+	List<Todo> selectList();
 
 	
 
